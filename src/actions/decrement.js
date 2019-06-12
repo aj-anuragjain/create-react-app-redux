bfcc32caf7d04e5c5fd5490b163928493ba8392f
Anurag Jain <ajsanuragjain@gmail.com>
2019-06-12 16:44:45 +0530

    segregate actions

---
+++ src/actions/decrement.js
@@ -0,0 +1,27 @@
+import { DECREMENT_REQUESTED, DECREMENT } from './actionTypes'
+
+export const decrement = () => {
+  return dispatch => {
+    dispatch({
+      type: DECREMENT_REQUESTED
+    })
+
+    dispatch({
+      type: DECREMENT
+    })
+  }
+}
+
+export const decrementAsync = () => {
+  return dispatch => {
+    dispatch({
+      type: DECREMENT_REQUESTED
+    })
+
+    return setTimeout(() => {
+      dispatch({
+        type: DECREMENT
+      })
+    }, 1500)
+  }
+}
---
+++ src/actions/increment.js
@@ -0,0 +1,27 @@
+import { INCREMENT_REQUESTED, INCREMENT } from './actionTypes'
+
+export const increment = () => {
+  return dispatch => {
+    dispatch({
+      type: INCREMENT_REQUESTED
+    })
+
+    dispatch({
+      type: INCREMENT
+    })
+  }
+}
+
+export const incrementAsync = () => {
+  return dispatch => {
+    dispatch({
+      type: INCREMENT_REQUESTED
+    })
+
+    return setTimeout(() => {
+      dispatch({
+        type: INCREMENT
+      })
+    }, 1500)
+  }
+}
